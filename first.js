92a15dffae8c7bb500621b75ea86ad2ba63a9449
Pradip Sable <96158817+PRADIP9193@users.noreply.github.com>
2022-12-02 15:49:38 +0530

    Add files via upload
    First.js 2/12/2011

---
+++ first.js
@@ -0,0 +1,29 @@
+// console.log("Hello, world!");
+// console.log(typeof 123124);
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log("Hello, world!");
+// const n=10;
+// for(let i=0;i<n;i++){
+//     console.log(i);
+// }
+// //conditional Statements
+// //error handling
+// try{
+//     throw new Error("pradip is an idiot");
+// }
+// catch(err){
+//     //console.log("Pradip is a nice guy.");
+//     console.log(err);
+// }
+
+// 
+
+// for(let i=0;i<100;i++){
+//     console
+//     .log("I love you Pradip");
+// }
+//npm init
+
